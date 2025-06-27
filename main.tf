77f882bb056d82ca6b2b39806611ff8a6b0eccf4
RJGcp <ravichanderkt@gmail.com>
2025-06-27 22:38:20 +0530

    Devops: VPC, SharedVPC, Resource creation

--- main.tf
+++ main.tf
@@ -1,23 +1,22 @@
-
 # [START vpc_shared_vpc_host_project_enable]
 resource "google_compute_shared_vpc_host_project" "host" {
-  project = var.project # Replace this with your host project ID in quotes
+  project = var.project
 }
 # [END vpc_shared_vpc_host_project_enable]
 
 # [START vpc_shared_vpc_service_project_attach]
 resource "google_compute_shared_vpc_service_project" "service1" {
   host_project    = google_compute_shared_vpc_host_project.host.project
-  service_project = var.service_project # Replace this with your service project ID in quotes
+  service_project = var.service_project
 }
 # [END vpc_shared_vpc_service_project_attach]
 
 # [START compute_shared_data_network]
 resource "google_compute_network" "network" {
-  name                      = "rj-oam"
-  project                   = var.project
-  auto_create_subnetworks   = false
-  mtu                       = 1460
+  name                    = "rj-oam"
+  project                 = var.project
+  auto_create_subnetworks = false
+  mtu                     = 1460
 }
 # [END compute_shared_data_network]
 
@@ -29,6 +28,16 @@ resource "google_compute_subnetwork" "subnet" {
   region        = "asia-south1"
 }
 # [END compute_shared_data_subnet]
+
+# ✅ Share only the rj-oam-mgmt subnet with the service project
+resource "google_compute_subnetwork_iam_member" "share_subnet" {
+  project    = var.project
+  region     = "asia-south1"
+  subnetwork = google_compute_subnetwork.subnet.name
+
+  role   = "roles/compute.networkUser"
+  member = "serviceAccount:service-${var.service_project_number}@compute-system.iam.gserviceaccount.com"
+}
 
 # [START compute_shared_internal_ip_create]
 resource "google_compute_address" "internal" {
@@ -49,7 +58,7 @@ resource "google_compute_instance" "reserved_ip" {
   machine_type = "e2-medium"
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20250610"
     }
   }
   network_interface {
@@ -67,7 +76,7 @@ resource "google_compute_instance" "ephemeral_ip" {
   machine_type = "e2-medium"
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20250610"
     }
   }
   network_interface {
@@ -76,7 +85,6 @@ resource "google_compute_instance" "ephemeral_ip" {
 }
 # [END compute_shared_instance_with_ephemeral_ip_create]
 
-
 # [START compute_shared_instance_template_create]
 resource "google_compute_instance_template" "default" {
   project      = var.service_project
@@ -84,34 +92,43 @@ resource "google_compute_instance_template" "default" {
   description  = "This template is used to create app server instances."
   machine_type = "n1-standard-1"
   disk {
-    source_image = "debian-cloud/debian-9"
+    source_image = "projects/debian-cloud/global/images/debian-12-bookworm-v20250610"
   }
   network_interface {
     subnetwork = google_compute_subnetwork.subnet.self_link
   }
+  
+  metadata = {
+    startup-script = file("${path.module}/startup.sh")
+  }
+
 }
 # [END compute_shared_instance_template_create]
 
 resource "google_compute_region_health_check" "default" {
-  name   = "l4-ilb-hc"
-  region = "europe-west1"
+  project = var.service_project
+  name    = "l4-ilb-hc"
+  region  = "asia-south1"
   http_health_check {
     port = "80"
   }
 }
+
 resource "google_compute_region_backend_service" "default" {
+  project               = var.service_project
   name                  = "l4-ilb-backend-subnet"
-  region                = "europe-west1"
+  region                = "asia-south1"
   protocol              = "TCP"
   load_balancing_scheme = "INTERNAL"
   health_checks         = [google_compute_region_health_check.default.id]
 }
+
 # [START compute_shared_forwarding_rule_l4_ilb]
 resource "google_compute_forwarding_rule" "default" {
   project               = var.service_project
   name                  = "l4-ilb-forwarding-rule"
   backend_service       = google_compute_region_backend_service.default.id
-  region                = "europe-west1"
+  region                = "asia-south1"
   ip_protocol           = "TCP"
   load_balancing_scheme = "INTERNAL"
   all_ports             = true
--- variables.tf
+++ variables.tf
@@ -5,3 +5,9 @@ variable "project" {
 variable "service_project" {
   description = "The service project ID"
 }
+
+variable "service_project_number" {
+  description = "The numeric ID of the service project"
+  type        = string
+}
+
