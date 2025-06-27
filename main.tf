3dd5eb9d2be0adf6709784622d8018e4554cee19
RJGcp <ravichanderkt@gmail.com>
2025-06-27 16:17:01 +0530

    Devops: VPC, SharedVPC, Resource creation

---
+++ main.tf
@@ -0,0 +1,122 @@
+
+# [START vpc_shared_vpc_host_project_enable]
+resource "google_compute_shared_vpc_host_project" "host" {
+  project = var.project # Replace this with your host project ID in quotes
+}
+# [END vpc_shared_vpc_host_project_enable]
+
+# [START vpc_shared_vpc_service_project_attach]
+resource "google_compute_shared_vpc_service_project" "service1" {
+  host_project    = google_compute_shared_vpc_host_project.host.project
+  service_project = var.service_project # Replace this with your service project ID in quotes
+}
+# [END vpc_shared_vpc_service_project_attach]
+
+# [START compute_shared_data_network]
+resource "google_compute_network" "network" {
+  name                      = "rj-oam"
+  project                   = var.project
+  auto_create_subnetworks   = false
+  mtu                       = 1460
+}
+# [END compute_shared_data_network]
+
+# [START compute_shared_data_subnet]
+resource "google_compute_subnetwork" "subnet" {
+  name          = "rj-oam-mgmt"
+  ip_cidr_range = "10.7.7.0/24"
+  network       = google_compute_network.network.self_link
+  region        = "asia-south1"
+}
+# [END compute_shared_data_subnet]
+
+# [START compute_shared_internal_ip_create]
+resource "google_compute_address" "internal" {
+  project      = var.service_project
+  region       = "asia-south1"
+  name         = "int-ip"
+  address_type = "INTERNAL"
+  address      = "10.7.7.8"
+  subnetwork   = google_compute_subnetwork.subnet.self_link
+}
+# [END compute_shared_internal_ip_create]
+
+# [START compute_shared_instance_with_reserved_ip_create]
+resource "google_compute_instance" "reserved_ip" {
+  project      = var.service_project
+  zone         = "asia-south1-a"
+  name         = "reserved-ip-instance"
+  machine_type = "e2-medium"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.self_link
+    network_ip = google_compute_address.internal.address
+  }
+}
+# [END compute_shared_instance_with_reserved_ip_create]
+
+# [START compute_shared_instance_with_ephemeral_ip_create]
+resource "google_compute_instance" "ephemeral_ip" {
+  project      = var.service_project
+  zone         = "asia-south1-a"
+  name         = "my-vm"
+  machine_type = "e2-medium"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.self_link
+  }
+}
+# [END compute_shared_instance_with_ephemeral_ip_create]
+
+
+# [START compute_shared_instance_template_create]
+resource "google_compute_instance_template" "default" {
+  project      = var.service_project
+  name         = "appserver-template"
+  description  = "This template is used to create app server instances."
+  machine_type = "n1-standard-1"
+  disk {
+    source_image = "debian-cloud/debian-9"
+  }
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.self_link
+  }
+}
+# [END compute_shared_instance_template_create]
+
+resource "google_compute_region_health_check" "default" {
+  name   = "l4-ilb-hc"
+  region = "europe-west1"
+  http_health_check {
+    port = "80"
+  }
+}
+resource "google_compute_region_backend_service" "default" {
+  name                  = "l4-ilb-backend-subnet"
+  region                = "europe-west1"
+  protocol              = "TCP"
+  load_balancing_scheme = "INTERNAL"
+  health_checks         = [google_compute_region_health_check.default.id]
+}
+# [START compute_shared_forwarding_rule_l4_ilb]
+resource "google_compute_forwarding_rule" "default" {
+  project               = var.service_project
+  name                  = "l4-ilb-forwarding-rule"
+  backend_service       = google_compute_region_backend_service.default.id
+  region                = "europe-west1"
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "INTERNAL"
+  all_ports             = true
+  allow_global_access   = true
+  network               = google_compute_network.network.self_link
+  subnetwork            = google_compute_subnetwork.subnet.self_link
+}
+# [END compute_shared_forwarding_rule_l4_ilb]
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "project" {
+  description = "The host project ID"
+}
+
+variable "service_project" {
+  description = "The service project ID"
+}
